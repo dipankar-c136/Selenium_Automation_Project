192e2f94037226145f5f3cc866195b589919bb32
dipankar_c <dc954725@gmail.com>
2025-01-14 00:49:34 +0530

    my new commit er

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
 C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s -m "regression"
 C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s testCases/test_login_ddt.py --browser chrome
+echo %PATH%
+python --version
+where python
+where pytest
