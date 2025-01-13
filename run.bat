f56f4c0f1e4c161d3741941259a057ea62410201
dipankar_c <dc954725@gmail.com>
2025-01-14 00:44:03 +0530

    my new commit w

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-pytest -v -s -m "regression"
-pytest -v -s testCases/test_login_ddt.py --browser chrome
+C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s -m "regression"
+C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s testCases/test_login_ddt.py --browser chrome
