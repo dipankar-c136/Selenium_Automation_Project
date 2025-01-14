9eb32b9bf1221321031493eaa33122f8e27d44b4
dipankar_c <dc954725@gmail.com>
2025-01-14 17:35:47 +0530

    my new commit errr

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
-C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s -m "regression"
-C:\Users\DIPANKAR\AppData\Roaming\Python\Python311\Scripts\pytest -v -s testCases/test_login_ddt.py --browser chrome
+pytest -v -s -m "regression"
+pytest -v -s testCases/test_login_ddt.py --browser chrome
 echo %PATH%
 python --version
 where python
 where pytest
+python -m site
